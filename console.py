9def1bbcb03c64ceb86b3467c69ab024f1108533
Kalob Taulien <kalob.taulien@gmail.com>
2018-10-15 18:59:31 -0600

    Seriously strong documentation

--- console.py
+++ console.py
@@ -1,15 +1,23 @@
+"""The magic begins here."""
+
 class Console(object):
+	"""This is your console object."""
 
     def log(self, message):
+    	"""Log simple error messages. Pretty standard JavaScripting."""
         print(message)
 
     def error(self, message):
+    	"""console.error() logging. Great for error reporting."""
         print(message)
 
     def warn(self, message):
+    	"""Need a warning? This is the method for you."""
         print(message)
 
     def debug(self, message):
+    	"""Looking to debug your code? console.debug('Your message') will solve all of your problems."""
         print(message)
+
 
 console = Console()
